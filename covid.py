c30e57240e09fa0b5b2c0d8a5dc14994b7923757
rodrigoforti2000 <67616733+rodrigoforti2000@users.noreply.github.com>
2020-08-07 00:10:07 -0300

    Update covid.py

--- covid.py
+++ covid.py
@@ -16,32 +16,32 @@ st.title("Covid-19")
 #ame_date_final = ameri_res.iloc[-1].date
 
 #tabela = pd.DataFrame(np.array([[ame_date_final, ame_total_cases, ame_total_deaths]]),
-                      columns=["Data", "Total Casos", "Total Mortes"])
+#                      columns=["Data", "Total Casos", "Total Mortes"])
 
 # Dados Brasil
-covid = pd.read_csv("https://raw.githubusercontent.com/peixebabel/COVID-19/master/data/casos-br-total.csv")
+#covid = pd.read_csv("https://raw.githubusercontent.com/peixebabel/COVID-19/master/data/casos-br-total.csv")
 
-m_diarias = np.array(covid["Mortes"]) - covid["Mortes"].shift()
-covid["mortes_diarias"] = m_diarias
+#m_diarias = np.array(covid["Mortes"]) - covid["Mortes"].shift()
+#covid["mortes_diarias"] = m_diarias
 
-tabela_br = pd.DataFrame(
-    np.array([[covid.iloc[-1].Data, covid.iloc[-1].Confirmados, int(covid.iloc[-1].Mortes), int(m_diarias.iloc[-1])]]),
-    columns=["Data", "Total Casos", "Total Mortes", "Ult. 24h"])
+#tabela_br = pd.DataFrame(
+#    np.array([[covid.iloc[-1].Data, covid.iloc[-1].Confirmados, int(covid.iloc[-1].Mortes), int(m_diarias.iloc[-1])]]),
+#    columns=["Data", "Total Casos", "Total Mortes", "Ult. 24h"])
 
-seq = range(len(covid))
-sns.relplot(y="mortes_diarias", x=seq, data=covid, kind="line")
-plt.title("Mortes Diárias por Covid-19 no Brasil", loc="left")
-plt.xlabel("Dias")
-plt.ylabel("Mortes Diárias")
-plt.xticks(
-    rotation=90,
-    fontweight='light',
-    horizontalalignment='right'
-)
-st.pyplot()
+#seq = range(len(covid))
+#sns.relplot(y="mortes_diarias", x=seq, data=covid, kind="line")
+#plt.title("Mortes Diárias por Covid-19 no Brasil", loc="left")
+#plt.xlabel("Dias")
+#plt.ylabel("Mortes Diárias")
+#plt.xticks(
+#    rotation=90,
+#    fontweight='light',
+#    horizontalalignment='right'
+#)
+#st.pyplot()
 
-st.subheader('Dados - Brasil')
-st.write(tabela_br)
+#st.subheader('Dados - Brasil')
+#st.write(tabela_br)
 
 #st.subheader('Dados - Americana')
 #st.write(tabela)
